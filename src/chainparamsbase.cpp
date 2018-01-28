57a85f693f4b5ea9e9a888e5e2f32a6a91a31350
breakcrypto <35466877+breakcrypto@users.noreply.github.com>
2018-01-28 13:37:20 -0500

    This updates the RPC port to 7111 from 9998 (#7)
    * Update RPC port
    
    * Update RPC
    
    We should remove this contrib though I think.
    
    * Update RPC
    
    updating the RPC
    
    * Update RPC
    
    ...
    
    * Update RPC reference
    
    * Update rpc port
    
    * Update RPC port

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9998;
+        nRPCPort = 7111;
     }
 };
 static CBaseMainParams mainParams;
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19998;
+        nRPCPort = 17111;
         strDataDir = "testnet3";
     }
 };
