aea636f86aa881fd9e6b9c4643a5f6e3b9c05f5a
barrystyle <barrystyle@westnet.com.au>
2019-09-07 04:16:38 +0800

    Add chiabls autocompilation step & warning regarding old g++ compiler.

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,25 @@
 # Copyright (c) 2013-2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 set -e
+
+currentver="$(gcc -dumpversion)"
+requiredver="6"
+if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then
+   echo "g++6 or higher detected"
+else
+   echo "your g++ is too old (g++5 released in 2015), please upgrade to minimum of 6"
+   exit
+fi
+
+unzip -o v20181101.zip
+cd bls-signatures-20181101
+mkdir -p build
+cd build
+cmake ..
+make -j2 install
+cd ../..
+
 srcdir="$(dirname $0)"
 cd "$srcdir"
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
