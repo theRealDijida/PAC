2f9d7d7d6f8bf861fde4d275f538cd6f9b491e5e
barrystyle <barrystyle@westnet.com.au>
2019-10-02 17:10:41 +0800

    revert 013169d63

--- src/governance.h
+++ src/governance.h
@@ -342,11 +342,7 @@ public:
         LOCK(cs);
         std::string strVersion;
         if (ser_action.ForRead()) {
-            Clear();
             READWRITE(strVersion);
-            if (strVersion != SERIALIZATION_VERSION_STRING) {
-                return;
-            }
         } else {
             strVersion = SERIALIZATION_VERSION_STRING;
             READWRITE(strVersion);
@@ -358,6 +354,10 @@ public:
         READWRITE(mapObjects);
         READWRITE(mapLastMasternodeObject);
         READWRITE(lastMNListForVotingKeys);
+        if (ser_action.ForRead() && (strVersion != SERIALIZATION_VERSION_STRING)) {
+            Clear();
+            return;
+        }
     }
 
     void UpdatedBlockTip(const CBlockIndex* pindex, CConnman& connman);
--- src/wallet/test/wallet_tests.cpp
+++ src/wallet/test/wallet_tests.cpp
@@ -351,7 +351,7 @@ BOOST_AUTO_TEST_CASE(ApproximateBestSubset)
 
     // Test vValue sort order
     for (int i = 0; i < 1000; i++)
-        add_coin(1000 * COIN);
+        add_coin(Params().GetConsensus().nMasternodeCollateral);
     add_coin(3 * COIN);
 
     BOOST_CHECK(testWallet.SelectCoinsMinConf(1003 * COIN, 1, 6, 0, vCoins, setCoinsRet, nValueRet));
@@ -378,7 +378,7 @@ BOOST_FIXTURE_TEST_CASE(rescan, TestChain100Setup)
         LOCK(wallet.cs_wallet);
         wallet.AddKeyPubKey(coinbaseKey, coinbaseKey.GetPubKey());
         BOOST_CHECK_EQUAL(oldTip, wallet.ScanForWalletTransactions(oldTip));
-        BOOST_CHECK_EQUAL(wallet.GetImmatureBalance(), 1000 * COIN);
+        BOOST_CHECK_EQUAL(wallet.GetImmatureBalance(), Params().GetConsensus().nMasternodeCollateral);
     }
 
     // Prune the older block file.
