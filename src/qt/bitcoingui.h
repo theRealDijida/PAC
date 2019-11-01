bfa3c76f3a2a291afa27a9fe9107cae76771edae
barrystyle <barrystyle@westnet.com.au>
2019-11-01 14:47:25 +0800

    Add governance tab with voting functions

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -102,6 +102,7 @@ private:
     QAction *overviewAction;
     QAction *historyAction;
     QAction *masternodeAction;
+    QAction *governanceAction;
     QAction *quitAction;
     QAction *sendCoinsAction;
     QAction *sendCoinsMenuAction;
@@ -239,6 +240,8 @@ private Q_SLOTS:
     void gotoHistoryPage();
     /** Switch to masternode page */
     void gotoMasternodePage();
+    /** Switch to governance page */
+    void gotoGovernancePage();
     /** Switch to receive coins page */
     void gotoReceiveCoinsPage();
     /** Switch to send coins page */
